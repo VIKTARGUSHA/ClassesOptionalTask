1d1e29666260c20ec5182c99b01953b3c73e97ed
Formation14 <paveltis@tut.by>
2022-01-18 16:10:29 +0300

    18.01.2021

--- src/ThirdTask/ThirdTask.java
+++ src/ThirdTask/ThirdTask.java
@@ -1,7 +1,9 @@
+package ThirdTask;
+
 import java.io.File;
 public class ThirdTask {
     public static void main(String[] args) {
-        File file = new File("C:\\Program Files\\ClassesMusic2");
+        File file = new File("C:\\Program Files\\Git");
         String [] filelist = file.list();
         for (String str : filelist){
             System.out.println(str);
